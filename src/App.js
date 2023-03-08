8831bf4217526a3996a0093325e4336adcf67b86
Brandon Roy <brandonronaldroy@gmail.com>
2023-03-08 17:44:35 -0400

    change 2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Change1
+          change2
         </p>
         <a
           className="App-link"
