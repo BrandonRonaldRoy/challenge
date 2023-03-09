b32996406a16ab2f4e338b2a2ea8ee5643a5f31a
Brandon Roy <brandonronaldroy@gmail.com>
2023-03-08 22:41:44 -0400

    added deployment scripts and homepage

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let answer1 = 2;
+  let answer2 = 25;
+  let answer3 = 'red';
+  let answer4 = 'blue'
+
+
   return (
     <div className="App">
+
+
+
+      <div>
+        <h1>Rebasing Challenge</h1>
+        <p>1 + 1 = {answer1}</p>
+        <p>50 / 2 = {answer2}</p>
+        <div>
+          <p>
+            Roses are {answer3} <br></br>
+            Violets are {answer4}  
+          </p>
+          
+          
+        </div>
+
+      </div>
+
+
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
